b2362805021cdd88b0abd050f7cb98974d25f740
Muhammad Kashaf <muhammadkashaf331@gmail.com>
2024-06-16 23:15:29 +0500

    sliding window (k) - (k-1)

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        ans= 0
-        for i in range(len(nums)):
-            for end in range(i,len(nums)):
-                sub_array = nums[i:end+1]
-                if sum(sub_array) == goal:
-                    ans+=1
-        return ans            
